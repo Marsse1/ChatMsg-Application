2e4d48e447a846ff3c560f3ecb21c8655461afdc
Marsel <marsel.bozhiqi@gmail.com>
2023-05-21 10:57:12 +0200

    First commit

---
+++ src/main/java/com/example/chatmsgr/ChatMsgRApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatmsgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatMsgRApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatMsgRApplication.class, args);
+    }
+
+}
